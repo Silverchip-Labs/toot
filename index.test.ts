c5e399c028a5e2695c2fa474834d9b6155a752b8
Rob Fairclough <rob@silverchip.com>
2023-02-17 14:04:08 +0000

    solutions for map, filter, begin reduce

--- index.test.ts
+++ index.test.ts
@@ -1,4 +1,4 @@
-import { coolMap } from "./index";
+import {coolMap, coolFilter, coolReduce} from "./index";
 
 describe("coolMap", () => {
     it("should map an array", () => {
@@ -7,3 +7,26 @@ describe("coolMap", () => {
     });
 });
 
+describe("coolFilter", () => {
+    it("returns an array", () => {
+        expect(coolFilter([], () => true)).toBeInstanceOf(Array);
+    })
+    it("filters an array", () => {
+        const fn = (el) => !!(el % 2);
+        const result = coolFilter([1, 2, 3], fn);
+        expect(result).toEqual([1, 3]);
+    })
+})
+
+describe("coolReduce", () => {
+    it('should return the sum of numbers when reducing a sum function on a number array', () => {
+        const actual = coolReduce([1, 2], (sum, n) => sum + n, 0);
+        const expected = 3;
+        expect(actual).toEqual(expected);
+    });
+    it('should use first element of collection as initial value if no accumulator is given', () => {
+        const actual = coolReduce([1, 2, 3], (sum, n) => sum + n);
+        const expected = 6;
+        expect(actual).toEqual(expected);
+    });
+})
--- index.ts
+++ index.ts
@@ -1,4 +1,22 @@
-export const coolMap = <TInput, TOutput> (arr: TInput[], fn: (item: TInput) => TOutput): TOutput[] => {
-    // todo
-    return [];
+export const coolMap = <TInput, TOutput> (arr: TInput[], fn: (item: TInput, index: number, array: TInput[]) => TOutput): TOutput[] => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++){
+        const element = arr[i];
+        const resultElement = fn(element, i, arr);
+        result.push(resultElement);
+    }
+
+    return result
+}
+
+export const coolFilter = <T> (arr: T[], fn: (item: T, index: number, array: T[]) => T): T[] => {
+    const result = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (fn(arr[i], i, arr)) result.push(arr[i]);
+    }
+    return result;
+}
+
+export const coolReduce = <TSource, TResult> (arr: TSource[], fn: (prevValue: TResult, curValue: TSource, index: number, array: TSource[]) => TResult, initialValue?: TResult | undefined) => {
+    return;
 }
